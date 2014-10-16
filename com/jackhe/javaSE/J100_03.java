9dc4e1777a2ba6d312b4b7312a2aaa7b44c1d567
JackHe <heshaoz_1990@163.com>
2014-10-16 17:23:35 +0800

    J100_03 completed

---
+++ com/jackhe/javaSE/J100_03.java
@@ -0,0 +1,21 @@
+/**
+
+calculate the sum of 1!+2!+..+10!
+
+Date:2014.10.16
+Author:JackHe
+*/
+//Sum
+public class J100_03{
+	public static void main(String args[]){
+	int i=0,j=0,sum=0;
+	for(i=1;i<=10;i++){
+	int product=1;
+	for(j=1;j<=i;j++){
+		product*=j;
+	}
+	sum+=product;
+		}
+	System.out.println("The sum of 1!+2!+..+10! is:\n"+sum);
+			}
+}
